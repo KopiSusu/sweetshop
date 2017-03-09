b4a4ee2971802b6a12f1a95135f574fcf6af5a85
Komang Luce <KopiSusu@Komangs-MBP.fios-router.home>
2017-03-09 12:05:31 -0500

    tab fixes

--- src/reducers/__tests__/inventory.selectors.test.js
+++ src/reducers/__tests__/inventory.selectors.test.js
@@ -3,31 +3,30 @@ import inventoryData from '../../data/inventory';
 
 describe('getInventory', () => {
 
-  	const initialState = {
-    	inventory: inventoryData['chocolates']
-  	};
+	const initialState = {
+		inventory: inventoryData['chocolates']
+	};
 
-  	it('should return the all items in inventory', () => {
-    	expect(getInventory(initialState, {})).toEqual(inventoryData['chocolates'])
-  	})
+	it('should return the all items in inventory', () => {
+		expect(getInventory(initialState, {})).toEqual(inventoryData['chocolates'])
+	})
 
 })
 
 describe('getProduct', () => {
 
-  	const initialState = {
-    	inventory: inventoryData['chocolates']
-  	};
+	const initialState = {
+		inventory: inventoryData['chocolates']
+	};
 
-  	it('should return the correct product from inventory', () => {
-    	expect(getProduct(initialState, {id: 1, quantity: 1})).toEqual({
-		      "description": "Milk chocolate milder and sweeter because it is made with milk and a higher sugar content than the darker varieties. It also has a smaller quantity of chocolate liquor and, therefore, fewer flavors and aromas.",
-		      "id": 1,
-		      "type": "milk",
-		      "quantity": 1,
-		      "price": 1.50
-		    }
-		)
-  	})
+	it('should return the correct product from inventory', () => {
+		expect(getProduct(initialState, {id: 1, quantity: 1})).toEqual({
+			"description": "Milk chocolate milder and sweeter because it is made with milk and a higher sugar content than the darker varieties. It also has a smaller quantity of chocolate liquor and, therefore, fewer flavors and aromas.",
+			"id": 1,
+			"type": "milk",
+			"quantity": 1,
+			"price": 1.50
+		})
+	})
 
 })
