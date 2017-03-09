b7f91d672c0511649b95cbbd81ef9de44e831c44
Komang Luce <KopiSusu@Komangs-MBP.fios-router.home>
2017-03-09 12:30:31 -0500

    forgot one

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,31 +1,31 @@
 import React, { Component, PropTypes } from 'react';
 
 class CartItem extends Component {
-    handleClick = () => {
-        const { id, removeFromCart } = this.props;
-        
-        removeFromCart(id);
-    }
+	handleClick = () => {
+		const { id, removeFromCart } = this.props;
+		
+		removeFromCart(id);
+	}
 
-    render() {
-      const {type, price, quantity} = this.props
+	render() {
+		const {type, price, quantity} = this.props
 
-      return (
-        <div className="cart__item">
-            <span className="cart__item__type">{type}</span>
-            <span className="cart__item__quantity">{quantity}</span>
-            <span className="cart__item__price">${price}</span>
-            <span className="cart__item__remove" onClick={this.handleClick}>Remove</span>
-        </div>
-      );
-    }
+		return (
+			<div className="cart__item">
+				<span className="cart__item__type">{type}</span>
+				<span className="cart__item__quantity">{quantity}</span>
+				<span className="cart__item__price">${price}</span>
+				<span className="cart__item__remove" onClick={this.handleClick}>Remove</span>
+			</div>
+		);
+	}
 }
 
 CartItem.propTypes = {
-    type: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    quantity: PropTypes.number.isRequired,
-    removeFromCart: PropTypes.func.isRequired
+	type: PropTypes.string.isRequired,
+	price: PropTypes.number.isRequired,
+	quantity: PropTypes.number.isRequired,
+	removeFromCart: PropTypes.func.isRequired
 }
 
 export default CartItem;
